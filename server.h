5b530d4ec994af0386504b2b07eb1bdd88e3103e
Infinity <573679956@qq.com>
2016-01-28 00:34:37 +0800

    edit server.h

--- server.h
+++ server.h
@@ -3,6 +3,9 @@
 
 #include <QObject>
 
+#include "tcpserver.h"
+#include "playerlist.h"
+
 class Server : public QObject
 {
     Q_OBJECT
@@ -10,9 +13,33 @@ public:
     explicit Server(QObject *parent = 0);
     ~Server();
 
+    void listen(int port);
+
+private:
+    static void info(QString message);
+
+    enum class Status
+    {
+        BEFORE_GAME_WAITING,
+        IN_GAME_PREPARING,
+        IN_GAME_DESCRIBING,
+        IN_GAME_DEALING_CARDS,
+        IN_GAME_SETTLING,
+        AFTER_GAME
+    };
+
+    Status status;
+    PlayerList playerList;
+
+protected:
+    TcpServer *server;
+
 signals:
 
 public slots:
+    void newClient(TcpSocket *socket);
+    void received(TcpSocket *socket, QString message);
+    void received(QVariant id, QString message);
 };
 
 #endif // SERVER_H
